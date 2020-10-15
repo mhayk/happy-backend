236504d5dca10aece57d30aef6a912e51d072d90
Mhayk Whandson <hi@mhayk.com>
2020-10-15 13:11:27 +0100

    The TypeORM has been configured.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
 
+import './database/connection'
+
 const app = express();
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    return res.send(req.body)
+})
 
 app.listen(3333);

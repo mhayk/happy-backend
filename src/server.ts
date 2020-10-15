8dd8e9d530c5ad47eba9bc58ab0eea1f28e33137
Mhayk Whandson <hi@mhayk.com>
2020-10-15 23:08:50 +0100

    The new endpoint "/orphanages" has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,40 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import orphanage from './models/Orphanage';
 
 import './database/connection'
+import Orphanage from './models/Orphanage';
 
 const app = express();
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
-    return res.send(req.body)
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage)
 })
 
 app.listen(3333);

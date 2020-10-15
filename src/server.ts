2119bcf58e7dc28f8af528acebb586788edab97f
Mhayk Whandson <hi@mhayk.com>
2020-10-15 23:22:08 +0100

    The application has been refactored to use MVC pattern.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import orphanage from './models/Orphanage';
+import routes from './routes'
 
 import './database/connection'
-import Orphanage from './models/Orphanage';
 
 const app = express();
 
 app.use(express.json())
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-    return response.status(201).json(orphanage)
-})
+app.use(routes)
 
 app.listen(3333);

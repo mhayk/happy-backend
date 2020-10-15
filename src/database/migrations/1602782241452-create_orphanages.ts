f37a433833173fd92157910accdc7fdbbe7133d0
Mhayk Whandson <hi@mhayk.com>
2020-10-15 22:53:00 +0100

    The new column opening_hours in the table orphanages has been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/database/migrations/1602782241452-create_orphanages.ts
+++ src/database/migrations/1602782241452-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602782241452 implements MigrationInterface {
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
@@ -48,7 +52,7 @@ export class createOrphanages1602782241452 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.dropTable('orphanages')
+    await queryRunner.dropTable('orphanages')
   }
 
 }

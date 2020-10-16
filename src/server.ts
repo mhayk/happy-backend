c1985ef079d3f5bc6bec0974f8db8a5d3adb8d28
Mhayk Whandson <hi@mhayk.com>
2020-10-16 12:59:31 +0100

    The endpoint /uploads has been created to allow to see the uploaded images.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes'
 
 import './database/connection'
@@ -7,5 +8,6 @@ const app = express();
 
 app.use(express.json())
 app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
